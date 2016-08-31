fb7e2ff6f9110b4c76b0b4e5d3554bbf6731a7d6
Mittsy Tidwell <mtidwell506@gmail.com>
2016-08-31 14:41:33 -0500

    added in notes and comments for foreach loop

--- foreach.php
+++ foreach.php
@@ -18,32 +18,61 @@ $things = array('Sgt. Pepper',
 
 // ================= DATA VALUE TYPE ======================
 foreach ($things as $value) {
+	// gets the data type for the value
 	echo gettype($value) . PHP_EOL;
 }
 echo PHP_EOL;
 
+
 // ================= DATA VALUE TYPE WITH SCALAR ======================
 foreach ($things as $value) {
+	// checks if value is scalar (float, integer, string, or boolean)
 	if (is_scalar($value)) {
 		if ($value == false) {
 			echo "false is scalar" . PHP_EOL;
+			// jumps out of loop and move to top of foreach loop
 			continue;
 		} echo "$value is scalar" . PHP_EOL;
 	}
 }
 echo PHP_EOL;
 
+
 // ================= ECHO VALUES ======================
 foreach ($things as $value) {
+	// prints out the $value type
+	echo gettype($value) . " ";
+	
+	// checks if $value is an array
 	if (is_array($value)) {
-		echo gettype($value) . " ";
 		
+		// starts looking at each item in nested array
 		foreach ($value as $index) {
-			echo $index. ", ";
+			echo $index . ", ";
+		// \x08 is a backspace
 		} echo "\x08\x08 \n";
+
 	} else {
 		echo $value . PHP_EOL;
 	}
 }
 echo PHP_EOL;
+
+
+// ================= ECHO VALUES WITH IMPLODE ======================
+foreach ($things as $value) {
+	// prints out the $value type
+	echo gettype($value) . " ";
+	
+	// checks if $value is an array
+	if (is_array($value)) {
+		// joins array element with a string
+		echo (implode(", ", $value));
+		echo "\x08\x08 \n";
+	} else {
+		echo $value . PHP_EOL;
+	}
+}
+echo PHP_EOL;
+
 
